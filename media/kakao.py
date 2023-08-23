b2fbe7d0a0a7a47abf58399fb161932a72236f4b
soonhwanlee <ecycle.hello@gmail.com>
2023-08-23 23:53:49 +0900

    [#4] 카카오 크롤링 구현 - api를 통해서 데이터 추출 - commentId의 경우 kakao에서 제공
    Todo
     - Test
     - log 작업
     - 에러처리
     - refactoring

--- media/kakao.py
+++ media/kakao.py
@@ -1,15 +1,122 @@
 from media.base import ScrapBase
+from utils.date_parser import *
+from utils.http_request import Get
 
 
 class KakaoPlace(ScrapBase):
+    headers = {
+        'User-Agent': 'Mozilla',
+        'content-type': 'application/json',
+    }
+
+    def __init__(self, store_id: str, store_name: str, status: str):
+        self.store_id = store_id
+        self.store_name = store_name
+
+        # N : 신규, P : 수집중, E : 수집 종료
+        self.status = status
+        self.store_info = {}
+        self.review_list = []
+
     def parse_html(self, text):
         pass
 
-    def get_reviews(self, place_id: str):
+    def get_reviews(self):
+
+        url = f'https://place.map.kakao.com/commentlist/v/{self.store_id}'
+        review_list = []
+        while True:
+            get = Get(url, self.headers)
+            data = get.request()
+            comment = data['comment']
+            has_next = comment['hasNext']
+            list = comment['list']
+            for each in range(list):
+                comment_id = each['commentId']
+                regdate = kakao_date_formatter(each['date'])
+                if self.status == 'P' and not is_yesterday(regdate):
+                    has_next = False
+                    break
+
+                if self.status == 's' and not is_two_years_before(regdate):
+                    has_next = False
+                    break
+
+                review = {
+                    'user_id': each['commentId'],
+                    'user_link': 'https://map.kakao.com/?target=other&tab=review&mapuserid=' + each['commentId'],
+                    'comment': each['contents'],
+                    'score': each['point'],
+                    'date': regdate,
+                    'comment_id': comment_id,
+                }
+                review_list.append(review)
+                url = f'https://place.map.kakao.com/commentlist/v/{self.store_id}/{comment_id}'
+
+            if not has_next:
+                break
+
+        return review_list
+
+    def get_comments(self):
         pass
 
-    def get_comments(self, place_id: str):
+    def get_place_basic_info(self):
+        url = f'https://place.map.kakao.com/main/v/{self.store_id}'
+        try:
+            get = Get(url, self.headers)
+            data = get.request()
+            api_place_info = data['basicInfo']
+            open_time, off_time = make_manage_hour(api_place_info['openHour'])
+            store_info = {
+                'place_id': api_place_info['cid'],
+                'description': api_place_info['description'],
+                'name': api_place_info['placenamefull'],
+                'geoX': api_place_info['wpointx'],
+                'geoY': api_place_info['wpointy'],
+                'address': api_place_info['address']['region']['fullname'] + ' ' + api_place_info['address']['addrbunho'],
+                'roadAddress': api_place_info['address']['newaddr']['newaddrfull'],
+                'phone': api_place_info['phonenum'],
+                'keywords': api_place_info['category']['catename'].split(','),
+                'thumbnail': api_place_info['mainphotourl'],
+                'link': api_place_info['homepage'],
+                'open_time': open_time,
+                'off_time': off_time
+            }
+
+            return store_info
+        except KeyError:
+            # Todo 에러처리
+            return
         pass
 
-    def get_place_basic_info(self, place_id: str):
-        pass
+    def attach_headers(self, headers: dict) -> None:
+        self.headers = headers
+
+
+def make_manage_hour(place_json):
+    if not place_json['openHour']:
+        return '', ''
+
+    open_hour = place_json['periodList']
+    period_list = []
+    if open_hour and len(open_hour) > 0:
+        period_name = open_hour['periodName']
+        time_list = open_hour["timeList"]
+        for entry in time_list:
+            time_name = entry.get("timeName", "")
+            time_se = entry.get("timeSE", "")
+            day_of_week = entry.get("dayOfWeek", "")
+            result = f"{period_name} : {time_name} {time_se} {day_of_week}"
+            period_list.append(result)
+
+    off_hour = place_json['offdayList']
+    off_day_list = []
+    if off_hour and len(off_hour) > 0:
+        for entry in time_list:
+            time_name = entry.get("holidayName", "")
+            time_se = entry.get("weekAndDay", "")
+            result = f"{time_name} : {time_se}"
+            off_day_list.append(result)
+
+    return ','.join(period_list), ','.join(off_day_list)
--- utils/date_parser.py
+++ utils/date_parser.py
@@ -17,11 +17,11 @@ def is_two_years_before(date_str):
 
 def is_yesterday(date_str):
     current_date = datetime.now()
-    two_years_ago = current_date - timedelta(days=1)
+    yester_day = current_date - timedelta(days=1)
 
     try:
         date_obj = naver_date_formatter(date_str)
-        if two_years_ago <= date_obj:
+        if yester_day <= date_obj:
             return True
     except ValueError:
         pass
@@ -32,4 +32,10 @@ def is_yesterday(date_str):
 def naver_date_formatter(date_str):
     formatted_date = date_str[0:-4]
     date_obj = datetime.strptime(formatted_date, '%Y년 %m월 %d일')
+    return date_obj
+
+
+def kakao_date_formatter(date_str):
+    formatted_date = date_str[0:-4]
+    date_obj = datetime.strptime(formatted_date, '%Y.%m.%d.')
     return date_obj
